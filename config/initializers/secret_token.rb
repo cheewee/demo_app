46c4a19f3468e97197eea6ec54c25fcbff523d13
rubyonrails <rubyonrails@ubuntu-rails.(none)>
2010-09-08 11:10:27 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'b8342c9ada300f5f0087f71d4b5f3a9300e97b6a0e9f2553d2a2688c45391c45256ec7313d67832b921882d83ba873392acb871e9b5dd3b96ea3a109d6f9cf8c'
